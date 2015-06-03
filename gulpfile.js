0055a34f1575d35dd0049a6b5bbcad36ef8dfd62
Cole Peters <cole@colepeters.com>
2015-06-04 00:15:25 +0100

    Some more starter setup

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@ var gulp = require('gulp');
 var cssnext = require('gulp-cssnext');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
+var rename = require('gulp-rename');
 
 gulp.task('css', function(){
   gulp.src('src/css/app.css')
     .pipe(cssnext({
       compress: true
+    }))
+    .pipe(rename({
+      suffix: '.min'
     }))
     .pipe(gulp.dest('./assets/css'))
     .pipe(reload({stream: true}));
