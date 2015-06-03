6b4e9f094c182e189ccb06ba175cebfdf410d0a6
Cole Peters <cole@colepeters.com>
2015-06-03 23:31:59 +0100

    Add npm modules and gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var cssnext = require('gulp-cssnext');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+gulp.task('css', function(){
+  gulp.src('src/css/app.css')
+    .pipe(cssnext({
+      compress: true
+    }))
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('sync', function(){
+  browserSync.init({
+    proxy: "localhost:2368"
+  });
+  gulp.watch('./src/css/*.css', ['css']);
+});
+
+gulp.task('default', ['css', 'sync']);
