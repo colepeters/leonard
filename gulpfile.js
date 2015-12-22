f07c5fb52027907dcd7c215383a06cb7f2b7c9ab
Cole Peters <cole@colepeters.com>
2015-12-22 10:54:02 -0600

    Move webfont script to head; standard for gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-var gulp = require('gulp');
-var cssnext = require('gulp-cssnext');
-var browserSync = require('browser-sync').create();
-var minifyCss = require('gulp-minify-css');
-var reload = browserSync.reload;
-var rename = require('gulp-rename');
+var gulp = require('gulp')
+var cssnext = require('gulp-cssnext')
+var browserSync = require('browser-sync').create()
+var minifyCss = require('gulp-minify-css')
+var reload = browserSync.reload
+var rename = require('gulp-rename')
 
-gulp.task('css', function(){
+gulp.task('css', function () {
   gulp.src('src/css/app.css')
     .pipe(cssnext({
       compress: false
@@ -15,21 +15,21 @@ gulp.task('css', function(){
       suffix: '.min'
     }))
     .pipe(gulp.dest('./assets/css'))
-    .pipe(reload({stream: true}));
-});
+    .pipe(reload({stream: true}))
+})
 
-gulp.task('sync', function(){
+gulp.task('sync', function () {
   browserSync.init({
-    proxy: "localhost:2368"
-  });
-  gulp.watch('./src/css/*.css', ['css']);
-  gulp.watch('./*.hbs').on('change', browserSync.reload);
-});
+    proxy: 'localhost:2368'
+  })
+  gulp.watch('./src/css/*.css', ['css'])
+  gulp.watch('./*.hbs').on('change', browserSync.reload)
+})
 
-gulp.task('build', ['css'], function(){
+gulp.task('build', ['css'], function () {
   gulp.src(['./**/*.hbs', '!./node_modules/**/*.hbs', 'assets/**/*', 'package.json'], {
     base: './'
-  }).pipe(gulp.dest('release'));
-});
+  }).pipe(gulp.dest('./release'))
+})
 
-gulp.task('default', ['css', 'sync']);
+gulp.task('default', ['css', 'sync'])
