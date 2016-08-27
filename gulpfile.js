87c2d44152598c20ba2b03e55dfd88d7eedfd45f
Cole Peters <cole@colepeters.com>
2016-08-27 12:49:15 +0100

    Ditch release dir, just copy/min CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-var gulp = require('gulp')
-var browserSync = require('browser-sync').create()
-var postcss = require('gulp-postcss')
-var rename = require('gulp-rename')
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var postcss = require('gulp-postcss');
+var rename = require('gulp-rename');
 
 var BROWSER_SUPPORT_LIST = [
   'Chrome >= 41',
@@ -11,9 +11,9 @@ var BROWSER_SUPPORT_LIST = [
   'iOS >= 8',
   'Android >= 4.4.2',
   'ChromeAndroid >= 4.4.2'
-]
+];
 
-gulp.task('css', function () {
+gulp.task('styles', function () {
   return gulp.src('src/css/app.css')
   .pipe(postcss([
     require('postcss-import'),
@@ -25,21 +25,16 @@ gulp.task('css', function () {
   .pipe(rename({
     suffix: '.min'
   }))
-  .pipe(gulp.dest('./assets/css'))
-})
+  .pipe(gulp.dest('./assets/css'));
+});
 
 gulp.task('sync', function () {
   browserSync.init({
     proxy: 'localhost:2368'
-  })
-  gulp.watch('./src/css/*.css', ['css'])
-  gulp.watch('./*.hbs').on('change', browserSync.reload)
-})
+  });
+  gulp.watch('./src/css/*.css', ['css']);
+  gulp.watch('./*.hbs').on('change', browserSync.reload);
+});
 
-gulp.task('build', ['css'], function () {
-  gulp.src(['./**/*.hbs', '!./node_modules/**/*.hbs', 'assets/**/*', 'package.json'], {
-    base: './'
-  }).pipe(gulp.dest('release'))
-})
-
-gulp.task('default', ['css', 'sync'])
+gulp.task('default', ['styles']);
+gulp.task('watch', ['styles', 'sync']);
